8bdfe5a731e4d7b6f6c18656b252144f17d43b9a
Shady Khalifa <shekohex@gmail.com>
2020-09-24 15:13:11 +0200

    tests should work again

--- ffi/tests/full.rs
+++ ffi/tests/full.rs
@@ -1,38 +1,41 @@
-use std::{env, path::PathBuf, process::Command};
+use std::{env, process::Command};
 
-fn build_full(out_dir_path: PathBuf) {
+fn build_full() {
     let cwd = env::current_dir().unwrap();
+    let workspace = cwd.parent().unwrap().to_path_buf();
     let mut cmd = Command::new("clang");
     #[cfg(not(windows))]
     let lib = "keystore";
     #[cfg(windows)]
     let lib = "keystore.dll";
-    let workspace = cwd.parent().unwrap().to_path_buf();
+    #[cfg(windows)]
+    let exe = "full.exe";
+    #[cfg(not(windows))]
+    let exe = "full";
     let lib = format!("-l{}", lib);
     let lib_dir = format!("-L{}", workspace.join("target/debug").display());
     let input = format!("{}", cwd.join("tests/full.c").display());
-    let output = format!("-o {}", out_dir_path.join("full").display());
-    dbg!(&[&input, &lib, &lib_dir, &output]);
+    let out = workspace.join("target").join("debug");
+    env::set_current_dir(out.as_path()).unwrap();
+    let output = format!("-o{}", exe);
     cmd.args(&[&input, &lib, &lib_dir, &output]);
     assert!(cmd.status().unwrap().success());
 }
 #[test]
 fn test_ffi() {
-    let out_path = tempfile::tempdir().unwrap().into_path();
-    build_full(out_path.as_path().into());
-    let mut cmd = Command::new(out_path.join("full"));
+    build_full();
+    let mut cmd = Command::new("full");
     let out = cmd.output().unwrap();
     let output = String::from_utf8_lossy(&out.stdout);
     println!("{}", output);
     assert!(out.status.success());
-    memory_test(out_path);
+    memory_test();
 }
 
 #[cfg(unix)]
-fn memory_test(out_dir_path: PathBuf) {
+fn memory_test() {
     let mut cmd = Command::new("valgrind");
-    cmd.args(&["--error-exitcode=1", "--leak-check=full"])
-        .arg(&*out_dir_path.join("full").to_string_lossy());
+    cmd.args(&["--error-exitcode=1", "--leak-check=full", "full"]);
     let out = cmd.output().unwrap();
     let output = String::from_utf8_lossy(&out.stdout);
     println!("{}", output);
@@ -40,6 +43,4 @@ fn memory_test(out_dir_path: PathBuf) {
 }
 
 #[cfg(windows)]
-fn memory_test(out_dir_path: PathBuf) {
-    let _ = out_dir_path;
-}
+fn memory_test() {}
